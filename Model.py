8a938a25a1aa036af10b5a90adb0da17841dc64c
MallikarjunaSS <mallikarjuna.sangannavar@gmail.com>
2023-06-24 16:05:21 +0530

    Added 3 new ML model code

--- Model.py
+++ Model.py
@@ -1,2 +1,11 @@
 
 print("Adding all model related code")
+
+def model() :
+    print("Adding code for ML model1")
+
+def model2() :
+    print("Adding code for ML model2")
+
+def model3() :
+    print("Adding code for ML model3")
